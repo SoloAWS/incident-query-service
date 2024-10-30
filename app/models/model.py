f879a5aa709987ef4690989abdb296db4080175a
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2024-10-29 19:57:53 -0500

    feat: added incident history

--- app/models/model.py
+++ app/models/model.py
@@ -1,5 +1,5 @@
 # model.py
-from sqlalchemy import Column, String, Enum, DateTime, LargeBinary
+from sqlalchemy import Column, String, Enum, DateTime, LargeBinary, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
@@ -21,3 +21,11 @@ class Incident(Base):
     manager_id = Column(UUID(as_uuid=True))
     file_data = Column(LargeBinary, nullable=True)
     file_name = Column(String, nullable=True)
+    
+class IncidentHistory(Base):
+    __tablename__ = "incident_history"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    incident_id = Column(UUID(as_uuid=True), ForeignKey("incidents.id"), nullable=False)
+    description = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
--- app/schemas/incident.py
+++ app/schemas/incident.py
@@ -2,7 +2,7 @@
 from pydantic import BaseModel, Field
 from uuid import UUID
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 from enum import Enum
 
 class IncidentState(str, Enum):
@@ -50,3 +50,21 @@ class IncidentDetailedResponse(BaseModel):
             datetime: lambda v: v.isoformat()
         }
 
+class IncidentHistoryResponse(BaseModel):
+    description: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
+
+class IncidentDetailedWithHistoryResponse(IncidentDetailedResponse):
+    history: List[IncidentHistoryResponse] = []
+
+    class Config:
+        from_attributes = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
